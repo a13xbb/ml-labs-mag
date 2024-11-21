a7f937fb4eb3f90e73db36730a791de2d474362b
a13xbb <alex.blokh24@gmail.com>
2024-11-22 01:45:22 +0300

    transforms in progress

--- lab3/utils.py
+++ lab3/utils.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import torch
+import torch.nn.functional as F
+import albumentations as A
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 
 def create_dataset(images_df: pd.DataFrame, annotations_df: pd.DataFrame) -> dict:
     train_dataset = []
@@ -21,3 +26,25 @@ def create_dataset(images_df: pd.DataFrame, annotations_df: pd.DataFrame) -> dic
             'annotations' : annotations_list
         })
     return train_dataset
+
+
+class LocalContrastNorm:
+    """Local Contrast Normalization"""
+    def __init__(self, kernel_size=3, eps=1e-5):
+        self.kernel_size = kernel_size
+        self.eps = eps
+        
+    def __call__(self, pic):
+        mean = F.avg_pool2d(pic, self.kernel_size, stride=1, padding=self.kernel_size//2)
+        std = torch.sqrt(F.avg_pool2d((pic - mean)**2, self.kernel_size, stride=1, padding=self.kernel_size//2) + self.eps)
+        std[std < 1] = 1
+        return (pic - mean) / std
+    
+
+def plot_image(image, bboxes, labels, classes_colors):
+    for i, bbox in enumerate(bboxes):
+        xmin, ymin, w, h = bbox
+        draw = ImageDraw.Draw(image)
+        draw.rectangle([xmin, ymin, xmin + w, ymin + h], outline=classes_colors[labels[i]], width=3)
+    plt.imshow(image)
+    plt.show()
