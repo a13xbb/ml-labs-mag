c6181f0bc3797280c6d6ddacfc9d21e094ca0c6a
a13xbb <alex.blokh24@gmail.com>
2024-11-23 16:38:42 +0300

    augmentations done

--- lab3/utils.py
+++ lab3/utils.py
@@ -4,6 +4,10 @@ import torch.nn.functional as F
 import albumentations as A
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
+from random import randint
+from torchvision.transforms import v2
+import torchvision.transforms as transforms
+import math
 
 def create_dataset(images_df: pd.DataFrame, annotations_df: pd.DataFrame) -> dict:
     train_dataset = []
@@ -48,3 +52,86 @@ def plot_image(image, bboxes, labels, classes_colors):
         draw.rectangle([xmin, ymin, xmin + w, ymin + h], outline=classes_colors[labels[i]], width=3)
     plt.imshow(image)
     plt.show()
+
+
+def rotate_point(point: tuple, old_img_size: tuple, new_img_size: tuple,  angle: float):
+    x, y = point
+    w, h = old_img_size
+    angle_rad = math.radians(-angle)
+    
+    cx, cy = w / 2, h / 2
+    
+    new_width, new_height = new_img_size
+    # new_width = int(abs(w * math.cos(angle_rad)) + abs(h * math.sin(angle_rad)))
+    # new_height = int(abs(w * math.sin(angle_rad)) + abs(h * math.cos(angle_rad)))
+    
+    new_cx = new_width / 2
+    new_cy = new_height / 2
+    
+    translated_x = x - cx
+    translated_y = y - cy
+    
+    rotated_x = translated_x * math.cos(angle_rad) - translated_y * math.sin(angle_rad)
+    rotated_y = translated_x * math.sin(angle_rad) + translated_y * math.cos(angle_rad)
+
+    final_x = rotated_x + new_cx
+    final_y = rotated_y + new_cy
+    
+    return final_x, final_y
+
+
+def rotate_bbox(bbox, old_img_size: tuple, new_img_size: tuple, angle: float):
+    x_min, y_min, w, h = bbox
+    points = [
+        (x_min, y_min),
+        (x_min, y_min + h),
+        (x_min + w, y_min + h),
+        (x_min + w, y_min)
+    ]
+    
+    rotated_x = []
+    rotated_y = []
+    for point in points:
+        r_x, r_y = rotate_point(point, old_img_size, new_img_size, angle)
+        rotated_x.append(r_x)
+        rotated_y.append(r_y)
+    
+    x_min = min(rotated_x)
+    y_min = min(rotated_y)
+    w = max(rotated_x) - x_min
+    h = max(rotated_y) - y_min
+    
+    return [x_min, y_min, w, h]
+
+
+# def BasicTransform(img, bboxes):
+#     # img  = v2.RandomHorizontalFlip()(img)
+#     # for i, bbox in enumerate(bboxes):
+#     #     bboxes[i] = torch.tensor(rotate_bbox(bbox, img.size, img.size, 180),
+#     #                              dtype=torch.float32)
+    
+#     angle = randint(-30, 30)
+#     r_image = img.rotate(angle, expand=1)
+#     for i, bbox in enumerate(bboxes):
+#         bboxes[i] = torch.tensor(rotate_bbox(bbox, img.size, r_image.size, angle),
+#                                  dtype=torch.float32)
+    
+#     r_image = v2.ColorJitter(brightness=(0.4, 2), contrast=(1, 6),
+#             saturation=(0, 2.5), hue=(-0.25,0.25))(r_image)
+    
+#     r_image = v2.RandomInvert(0.3)(r_image)
+    
+#     resized_image = transforms.Resize((224, 224))(r_image)
+#     w_new, h_new = resized_image.size
+#     w_old, h_old = r_image.size
+#     scale_x = w_new / w_old
+#     scale_y = h_new / h_old
+#     for i, bbox in enumerate(bboxes):
+#         x_old, y_old, w_old, h_old = bbox
+#         bboxes[i] = torch.tensor([x_old * scale_x, y_old * scale_y, w_old * scale_x, h_old * scale_y],
+#                                     dtype=torch.float32)
+    
+#     r_image = transforms.ToTensor()
+    
+#     return r_image, bboxes
+        
